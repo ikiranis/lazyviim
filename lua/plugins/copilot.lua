460ce3d5bb9731b54f1091587ef7083add70568c
rocean <rocean74@gmail.com>
2024-11-22 01:17:18 +0200

    first commit

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+-- Copilot autocomplete plugins
+
+return {
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<A-CR>", 'copilot#Accept("<CR>")', { silent = true, expr = true, noremap = true })
+  end,
+}
