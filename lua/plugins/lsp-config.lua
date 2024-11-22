eeb54e3873ba922b9316d0556fd78b81070c260e
rocean <rocean74@gmail.com>
2024-11-23 00:54:12 +0200

    Add new plugins

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,16 @@
+return {
+    "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+            phpactor = {
+                enabled = lsp == "phpactor",
+            },
+            intelephense = {
+                enabled = lsp == "intelephense",
+            },
+            lsp = {
+                enabled = true,
+            },
+        },
+    },
+}
